5ffc60a5b709a0222af3c406fbb3d15a607773c1
Aidan Baack <aebaack@gmail.com>
2018-11-27 16:57:44 -0700

    Move data into workable dataframe

--- RProj.R
+++ RProj.R
@@ -5,7 +5,8 @@ emerging_data = read.csv('EEM.csv')
 
 # Split data into quarters of the year
 
-library(lubricate)
+install.packages("lubridate", dependencies=TRUE, repos='http://cran.rstudio.com/')
+library(lubridate)
 
 sp_quarters = split(sp_data, quarter(sp_data$Date, with_year=TRUE))
 emerging_quarters = split(emerging_data, quarter(emerging_data$Date, with_year=TRUE))
@@ -16,6 +17,14 @@ calculate_cv = function(vec) {
   return (sd(vec$Adj.Close) / mean(vec$Adj.Close))
 }
 
+# Calculate cv for each quarter and combine into a single dataframe
 sp_cv = lapply(sp_quarters, calculate_cv)
-emerging_cv = lapply(sp_quarters, calculate_cv)
+sp_cv = data.frame(
+  Quarter = attr(sp_quarters, 'names'), 
+  CV = as.data.frame(do.call("rbind", sp_cv))$V1)
+
+emerging_cv = lapply(emerging_quarters, calculate_cv)
+emerging_cv = data.frame(
+  Quarter = attr(emerging_quarters, 'names'), 
+  CV = as.data.frame(do.call("rbind", emerging_cv))$V1)
 
