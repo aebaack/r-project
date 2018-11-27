1da11e25a2394da5628cef14bdef08a204b85965
Aidan Baack <aebaack@gmail.com>
2018-11-27 16:17:34 -0700

    Calculate the coefficient of variation for each quarter

--- RProj.R
+++ RProj.R
@@ -1,4 +1,4 @@
-# Import data
+# Import stock data
 
 sp_data = read.csv('SP.csv')
 emerging_data = read.csv('EEM.csv')
@@ -6,5 +6,16 @@ emerging_data = read.csv('EEM.csv')
 # Split data into quarters of the year
 
 library(lubricate)
+
 sp_quarters = split(sp_data, quarter(sp_data$Date, with_year=TRUE))
 emerging_quarters = split(emerging_data, quarter(emerging_data$Date, with_year=TRUE))
+
+# Analyze the coefficient of variation
+
+calculate_cv = function(vec) {
+  return (sd(vec$Adj.Close) / mean(vec$Adj.Close))
+}
+
+sp_cv = lapply(sp_quarters, calculate_cv)
+emerging_cv = lapply(sp_quarters, calculate_cv)
+
